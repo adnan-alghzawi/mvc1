ea314ed6a8d594b53a0280f7df0a96d6e1790249
adnan alghzawi <adnanalghzawi7@gmail.com>
2025-02-16 22:04:06 +0300

    Add project files.

---
+++ mvc1/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc1.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvc1/Controllers/SubscribeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc1.Controllers
+{
+    public class SubscribeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Subscribe()
+        {
+            return View();
+        }
+    }
+}
